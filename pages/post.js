98197a52f493973d45111c25c713d0e140d497a3
matt-winzer <matt.winzer@gmail.com>
2019-05-29 15:22:59 -0600

    refactored to use getInitialProps

--- pages/post.js
+++ pages/post.js
@@ -1,14 +1,14 @@
-import { withRouter } from 'next/router';
-import Layout from '../components/Layout.js';
+import { withRouter } from 'next/router'
+import Layout from '../components/Layout.js'
 
 const Content = withRouter(props => (
   <div>
     <h1>{props.router.query.title}</h1>
-    <p>This is the blog post content.</p>
+    <p>Page content.</p>
   </div>
 ))
 
-const Page = props => (
+const Page = () => (
   <Layout>
     <Content />
   </Layout>
